3f4c4fbc6bf56b323d1b1336658712261b2ee68c
Ziyan Li <liziyan1992@gmail.com>
2023-10-30 20:41:24 +0800

    book stay - submit button

--- staybooking0/src/components/GustHomePage.js
+++ staybooking0/src/components/GustHomePage.js
@@ -11,13 +11,106 @@ import {
   Button,
   Card,
   Carousel,
+  Modal,
 } from "antd";
-import { getReservations, searchStays } from "../utils";
+import { bookStay, getReservations, searchStays } from "../utils";
 import { LeftCircleFilled, RightCircleFilled } from "@ant-design/icons";
 import { StayDetailInfoButton } from "./HostHomePage";
 
 const { TabPane } = Tabs;
 const { Text } = Typography;
+
+class BookStayButton extends React.Component {
+  state = {
+    loading: false,
+    modalVisible: false,
+  };
+
+  handleCancel = () => {
+    this.setState({
+      modalVisible: false,
+    });
+  };
+
+  handleBookStay = () => {
+    this.setState({
+      modalVisible: true,
+    });
+  };
+
+  handleSubmit = async (values) => {
+    const { stay } = this.props;
+    this.setState({
+      loading: true,
+    });
+
+    try {
+      await bookStay({
+        checkin_date: values.checkin_date.format("YYYY-MM-DD"),
+        checkout_date: values.checkout_date.format("YYYY-MM-DD"),
+        stay: {
+          id: stay.id,
+        },
+      });
+      message.success("Successfully book stay");
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  };
+
+  render() {
+    const { stay } = this.props;
+    return (
+      <>
+        <Button onClick={this.handleBookStay} shape="round" type="primary">
+          Book Stay
+        </Button>
+        <Modal
+          destroyOnClose={true}
+          title={stay.name}
+          visible={this.state.modalVisible}
+          footer={null}
+          onCancel={this.handleCancel}
+        >
+          <Form
+            preserve={false}
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            onFinish={this.handleSubmit}
+          >
+            <Form.Item
+              label="Checkin Date"
+              name="checkin_date"
+              rules={[{ required: true }]}
+            >
+              <DatePicker />
+            </Form.Item>
+            <Form.Item
+              label="Checkout Date"
+              name="checkout_date"
+              rules={[{ required: true }]}
+            >
+              <DatePicker />
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button
+                loading={this.state.loading}
+                type="primary"
+                htmlType="submit"
+              >
+                Book
+              </Button>
+            </Form.Item>
+          </Form>
+        </Modal>
+      </>
+    );
+  }
+}
 
 class SearchStays extends React.Component {
   state = {
@@ -104,7 +197,7 @@ class SearchStays extends React.Component {
                     <StayDetailInfoButton stay={item} />
                   </div>
                 }
-                extra={null}
+                extra={<BookStayButton stay={item} />}
               >
                 {
                   <Carousel
